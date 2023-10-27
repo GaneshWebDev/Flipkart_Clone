3793066d263bb492b32680293ea247ef1801b12f
Ganesh_web_dev <115787522+GaneshWebDev@users.noreply.github.com>
2023-10-27 13:20:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const products=require('./routes/products');
 const userSignUp=require('./routes/userRoute');
 const cors=require('cors');
 const app=express();
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://flikart-clone-frontend.onrender.com' }));
 app.use(express.json());
 app.use(userSignUp);
 app.use(products);
