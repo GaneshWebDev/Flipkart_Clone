7e9f95207caeba4aa3f714635b5fb1d841ad9e52
Ganesh_web_dev <115787522+GaneshWebDev@users.noreply.github.com>
2023-11-07 14:14:19 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,21 +4,9 @@ const products=require('./routes/products');
 const userSignUp=require('./routes/userRoute');
 const cors=require('cors');
 const app=express();
-/*app.use(cors({
+app.use(cors({
   origin: 'https://flikart-clone-frontend.onrender.com', // Include cookies in the requests if applicable
-}));*/
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // Check if the origin of the request is included in the allowedOrigins array
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  })
-);
+}));
 app.use(express.json());
 app.use(userSignUp);
 app.use(products);
